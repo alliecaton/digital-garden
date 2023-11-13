afd7bb843afa14ef5541d5bbfabd68bd70c1ace0
alliecaton <alliecaton3@gmail.com>
2023-11-12 18:50:52 -0800

    Added edit & delete functionality

--- src/components/PostCreate.vue
+++ src/components/PostCreate.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-import { mergeProps, ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
+
+import Loader from '@/components/Loader.vue'
+
+import type { Post } from '@/types/Posts'
 
 import fetch from '@/utils/fetch'
 
@@ -7,17 +11,28 @@ const props = defineProps<{
   submitToken?: string
 }>()
 
+const id = ref<number | null>(null)
 const title = ref('')
 const content = ref('')
-const postId = ref(null)
 
-const created = ref(false)
-const create = async () => {
-  created.value = false
+const method = ref('post')
+
+const success = ref(false)
+const createOrUpdate = async () => {
+  const isPut = Boolean(method.value === 'put')
+
+  let path = '/posts'
+
+  if (isPut) {
+    path = `/posts/${id.value}`
+  }
+
+  success.value = false
+
   try {
     const data = await fetch({
-      method: 'post',
-      path: '/post',
+      method: method.value,
+      path: path,
       data: {
         title: title.value,
         content: content.value,
@@ -28,19 +43,90 @@ const create = async () => {
     })
 
     if (data) {
-      created.value = true
-      postId
+      success.value = true
     }
   } catch (e) {
     console.error(e)
+  } finally {
+    getPosts()
   }
 }
+
+const deletePost = async (post: Post) => {
+  success.value = false
+  try {
+    const data = await fetch({
+      method: 'delete',
+      path: `/posts/${post.id}`,
+      headers: {
+        'x-access-token': props.submitToken,
+      },
+    })
+
+    if (data) {
+      success.value = true
+    }
+  } catch (e) {
+    console.error(e)
+  } finally {
+    getPosts()
+  }
+}
+
+const loadingPosts = ref(true)
+const posts = ref<Post[]>([])
+const getPosts = async () => {
+  loadingPosts.value = true
+  try {
+    const data = await fetch({
+      method: 'get',
+      path: '/posts',
+    })
+
+    if (data) {
+      posts.value = data
+    }
+  } catch (e) {
+    console.error(e)
+  } finally {
+    loadingPosts.value = false
+  }
+}
+
+onMounted(() => {
+  getPosts()
+})
+
+const populateNewPost = () => {
+  title.value = ''
+  content.value = ''
+  id.value = null
+
+  method.value = 'post'
+}
+
+const selectPost = (post: Post) => {
+  title.value = post.title
+  content.value = post.content
+  id.value = post.id
+
+  method.value = 'put'
+}
+
+const submitButtonText = computed(() => {
+  if (method.value === 'post') return 'submit new post'
+
+  return 'update post'
+})
 </script>
 
 <template>
   <div class="container">
-    <div class="success" v-if="created">successfully posted!!</div>
-    <router-link :to="'/posts/' + postId">{{ title }}</router-link>
+    <div class="success" v-if="success">successful {{ method }}!!</div>
+
+    <button v-if="method === 'put'" @click="populateNewPost">
+      create new post
+    </button>
 
     <div>
       <label>title:</label>
@@ -54,7 +140,17 @@ const create = async () => {
       <textarea type="textarea" rows="20" v-model="content"></textarea>
     </div>
 
-    <button @click="create">submit</button>
+    <button @click="createOrUpdate">{{ submitButtonText }}</button>
+
+    <div class="posts" v-if="!loadingPosts">
+      <div class="post" v-for="post in posts" :key="post.id">
+        <span class="post__delete" @click="() => deletePost(post)">🚮</span>
+        <span class="post__title" @click="() => selectPost(post)">{{
+          post.title
+        }}</span>
+      </div>
+    </div>
+    <Loader v-else />
   </div>
 </template>
 
@@ -62,6 +158,7 @@ const create = async () => {
 .success {
   font-weight: bold;
   color: $turq;
+  margin-bottom: 20px;
 }
 
 .content {
@@ -72,5 +169,24 @@ input,
 textarea {
   width: 100%;
   padding: 10px;
+}
+
+.posts {
+  border-top: 1px solid black;
+  padding-top: 20px;
+  margin-top: 30px;
+}
+
+.post {
+  cursor: pointer;
+}
+
+.post__title {
+  text-decoration: underline;
+}
+
+.post__delete {
+  padding: 10px;
+  text-decoration: none;
 }
 </style>

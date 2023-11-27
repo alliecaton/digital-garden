0b5bb0fca8fb492e022136c77891c7fb0d466376
alliecaton <alliecaton3@gmail.com>
2023-11-27 15:02:33 -0800

    Fixed auth bug

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -7,28 +7,51 @@ export const useAuthStore = defineStore({
   state: () => ({
     // @ts-ignore-next-line
     token: JSON.parse(localStorage.getItem('token')),
+    loggedIn: false,
   }),
   actions: {
-    async getUser() {},
+    async checkUser() {
+      try {
+        const res = await fetch({
+          method: 'get',
+          path: '/user',
+        })
+
+        if (res) {
+          this.loggedIn = res.loggedIn
+        } else {
+          this.loggedIn = false
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    },
     async login(username: string, password: string) {
-      const res = await fetch({
-        method: 'post',
-        path: '/login',
-        data: {
-          username: username,
-          password: password,
-        },
-      })
+      try {
+        const res = await fetch({
+          method: 'post',
+          path: '/login',
+          data: {
+            username: username,
+            password: password,
+          },
+        })
 
-      // update pinia state
-      this.token = res.accessToken
+        // update pinia state
+        this.token = res.accessToken
 
-      // store user details and jwt in local storage to keep user logged in between page refreshes
-      localStorage.setItem('token', JSON.stringify(this.token))
+        // store user details and jwt in local storage to keep user logged in between page refreshes
+        localStorage.setItem('token', JSON.stringify(this.token))
+
+        this.loggedIn = true
+      } catch (e) {
+        console.error(e)
+      }
     },
     logout() {
       this.token = null
       localStorage.removeItem('token')
+      this.loggedIn = false
     },
   },
 })
